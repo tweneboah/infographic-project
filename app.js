9dc31d84c244f40ef76a34ad37592a67896649e4
Emmanuel <twentekghana@gmail.com>
2020-07-28 06:41:04 +0800

    Constructor function for Dino created

--- app.js
+++ app.js
@@ -1,32 +1,47 @@
+// Create Dino Constructor
+/**
+ * @param  {Dino's species} species
+ * @param  {Dino's weight} weight
+ * @param  {Dino's height} height
+ * @param  {Dino's diet} diet
+ * @param  {Where the Dino comes from} where
+ * @param  {date of the Dino} when
+ * @param  {fact about the dino} fact
+ * @param {desc} Constructor
+ */
+function Dino(species, weight, height, diet, where, when, fact) {
+  this.species = species;
+  this.weight = weight;
+  this.height = height;
+  this.diet = diet;
+  this.wwhere = where;
+  this.when = when;
+  this.fact = fact;
+}
 
-    // Create Dino Constructor
+// Create Dino Objects
+const dino1 = new Dino();
 
+// Create Human Object
 
-    // Create Dino Objects
+// Use IIFE to get human data from form
 
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Create Human Object
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Use IIFE to get human data from form
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
+// Generate Tiles for each Dino in Array
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+// Add tiles to DOM
 
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
-
-    // Remove form from screen
-
+// Remove form from screen
 
 // On button click, prepare and display infographic
+
+
+
